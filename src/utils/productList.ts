c7e8843745ba801dc722414e78aba0a362fc338f
Alistair Abao <abaolester@gmail.com>
2026-09-22 23:18:52 +0200

    added layout for the product card

--- src/utils/productList.ts
+++ src/utils/productList.ts
@@ -9,6 +9,8 @@ export const products = [
     shipping:
       "Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.",
     volume: 21,
+    imageUrl:
+      "https://images.unsplash.com/photo-1580870069867-74c57ee1bb07?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29zbWV0aWMlMjBwcm9kdWN0c3xlbnwwfHwwfHx8MA%3D%3D",
   },
   {
     id: { $oid: "6ab1a07ffc13ae25d535bac3" },
@@ -19,6 +21,8 @@ export const products = [
     shipping:
       "In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.\n\nCras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
     volume: 81,
+    imageUrl:
+      "https://images.unsplash.com/photo-1583209814683-c023dd293cc6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y29zbWV0aWMlMjBwcm9kdWN0c3xlbnwwfHwwfHx8MA%3D%3D",
   },
   {
     id: { $oid: "6ab1a07ffc13ae25d535bac4" },
@@ -29,6 +33,8 @@ export const products = [
     shipping:
       "Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.\n\nNullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.",
     volume: 82,
+    imageUrl:
+      "https://images.unsplash.com/photo-1629198688000-71f23e745b6e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNvc21ldGljJTIwcHJvZHVjdHN8ZW58MHx8MHx8fDA%3D",
   },
   {
     id: { $oid: "6ab1a07ffc13ae25d535bac5" },
@@ -39,6 +45,8 @@ export const products = [
     shipping:
       "Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.\n\nMorbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.\n\nFusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.",
     volume: 45,
+    imageUrl:
+      "https://images.unsplash.com/photo-1598528738936-c50861cc75a9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNvc21ldGljJTIwcHJvZHVjdHN8ZW58MHx8MHx8fDA%3D",
   },
   {
     id: { $oid: "6ab1a07ffc13ae25d535bac6" },
@@ -49,6 +57,8 @@ export const products = [
     shipping:
       "Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
     volume: 64,
+    imageUrl:
+      "https://images.unsplash.com/photo-1612817288484-6f916006741a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGNvc21ldGljJTIwcHJvZHVjdHN8ZW58MHx8MHx8fDA%3D",
   },
   {
     id: { $oid: "6ab1a07ffc13ae25d535bac7" },
@@ -59,6 +69,8 @@ export const products = [
     shipping:
       "Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.",
     volume: 14,
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1670537994863-5ad53a3214e0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGNvc21ldGljJTIwcHJvZHVjdHN8ZW58MHx8MHx8fDA%3D",
   },
   {
     id: { $oid: "6ab1a07ffc13ae25d535bac8" },
@@ -68,6 +80,8 @@ export const products = [
     ingredients: ["flour", "eggs", "vanilla extract"],
     shipping: "Fusce consequat. Nulla nisl. Nunc nisl.",
     volume: 18,
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1661726457110-c43a88d74567?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGNvc21ldGljJTIwcHJvZHVjdHN8ZW58MHx8MHx8fDA%3D",
   },
   {
     id: { $oid: "6ab1a07ffc13ae25d535bac9" },
@@ -78,6 +92,8 @@ export const products = [
     shipping:
       "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.\n\nMaecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.\n\nNullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.",
     volume: 100,
+    imageUrl:
+      "https://images.unsplash.com/photo-1522335789203-aabd1fc54bc9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y29zbWV0aWMlMjBwcm9kdWN0c3xlbnwwfHwwfHx8MA%3D%3D",
   },
   {
     id: { $oid: "6ab1a07ffc13ae25d535baca" },
@@ -89,6 +105,8 @@ export const products = [
     shipping:
       "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
     volume: 20,
+    imageUrl:
+      "https://images.unsplash.com/photo-1596704017254-9b121068fb31?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGNvc21ldGljJTIwcHJvZHVjdHN8ZW58MHx8MHx8fDA%3D",
   },
   {
     id: { $oid: "6ab1a07ffc13ae25d535bacb" },
@@ -99,6 +117,8 @@ export const products = [
     shipping:
       "Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.",
     volume: 30,
+    imageUrl:
+      "https://images.unsplash.com/photo-1631730486572-226d1f595b68?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNvc21ldGljJTIwcHJvZHVjdHN8ZW58MHx8MHx8fDA%3D",
   },
   {
     id: { $oid: "6ab1a07ffc13ae25d535bacc" },
@@ -109,6 +129,8 @@ export const products = [
     shipping:
       "Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.\n\nPhasellus in felis. Donec semper sapien a libero. Nam dui.",
     volume: 4,
+    imageUrl:
+      "https://images.unsplash.com/photo-1631730486572-226d1f595b68?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNvc21ldGljJTIwcHJvZHVjdHN8ZW58MHx8MHx8fDA%3D",
   },
   {
     id: { $oid: "6ab1a07ffc13ae25d535bacd" },
@@ -119,6 +141,8 @@ export const products = [
     shipping:
       "Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.\n\nCras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n\nProin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.",
     volume: 88,
+    imageUrl:
+      "https://images.unsplash.com/photo-1596462502278-27bfdc403348?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29zbWV0aWMlMjBwcm9kdWN0c3xlbnwwfHwwfHx8MA%3D%3D",
   },
   {
     id: { $oid: "6ab1a07ffc13ae25d535bace" },
@@ -129,6 +153,8 @@ export const products = [
     shipping:
       "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
     volume: 24,
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1684407616442-8d5a1b7c978e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y29zbWV0aWMlMjBwcm9kdWN0c3xlbnwwfHwwfHx8MA%3D%3D",
   },
   {
     id: { $oid: "6ab1a07ffc13ae25d535bacf" },
@@ -139,6 +165,8 @@ export const products = [
     shipping:
       "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.\n\nIn sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.",
     volume: 72,
+    imageUrl:
+      "https://images.unsplash.com/photo-1619451427882-6aaaded0cc61?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29zbWV0aWMlMjBwcm9kdWN0c3xlbnwwfHwwfHx8MA%3D%3D",
   },
   {
     id: { $oid: "6ab1a07ffc13ae25d535bad0" },
@@ -149,5 +177,7 @@ export const products = [
     shipping:
       "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.",
     volume: 74,
+    imageUrl:
+      "https://images.unsplash.com/photo-1620916566398-39f1143ab7be?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvc21ldGljJTIwcHJvZHVjdHN8ZW58MHx8MHx8fDA%3D",
   },
 ];
